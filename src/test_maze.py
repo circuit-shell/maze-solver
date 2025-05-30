184a1bf9e331ca21eebd1617dfefb0ab39959151
Armando del Río <adrio.6180@gmail.com>
2025-05-29 18:02:29 -0600

    feat: add test

---
+++ src/test_maze.py
@@ -0,0 +1,18 @@
+from maze import Maze
+import unittest
+
+
+class Tests(unittest.TestCase):
+    def test_maze_create_cells(self):
+        num_cols = 12
+        num_rows = 10
+        self.assertEqual(0,0)
+        # m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        # self.assertEqual(
+        #     len(m1._Maze__cells),
+        #     num_cols,
+        # )
+        # self.assertEqual(
+        #     len(m1._Maze__cells[0]),
+        #     num_rows,
+        # )
