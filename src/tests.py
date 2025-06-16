48cc932a7373d4ab27158700c9804a9bab97c658
Armando del Río <adrio.6180@gmail.com>
2025-06-15 22:41:28 -0600

    feat: Reset Visited method

--- src/tests.py
+++ src/tests.py
@@ -8,30 +8,38 @@ class Tests(unittest.TestCase):
         num_cols = 12
         num_rows = 10
         m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
-        cells = m1.get_cells()
         self.assertEqual(
-            len(cells),
+            len(m1._Maze__cells),
             num_cols,
         )
         self.assertEqual(
-            len(cells[0]),
-
+            len(m1._Maze__cells[0]),
             num_rows,
         )
 
-    def test_maze_create_cells_large(self):
-        num_cols = 16
-        num_rows = 12
+    def test_maze_break_entrance_and_exit(self):
+        num_cols = 12
+        num_rows = 10
         m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
-        cells = m1.get_cells()
         self.assertEqual(
-            len(cells),
-            num_cols,
+            m1._Maze__cells[0][0].has_top_wall,
+            False,
         )
         self.assertEqual(
-            len(cells[0]),
-            num_rows,
+            m1._Maze__cells[num_cols - 1][num_rows - 1].has_bottom_wall,
+            False,
         )
+
+    def test_maze_reset_cells_visited(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        for col in m1._Maze__cells:
+            for cell in col:
+                self.assertEqual(
+                    cell.visited,
+                    False,
+                )
 
 
 if __name__ == "__main__":
